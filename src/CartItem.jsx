0212f0b9cbe6969a4eba724a875d5f3f032a4cbf
pouloune <dasseshella@gmail.com>
2024-09-10 13:45:47 -0400

    increment, decrement, to fix delete

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -31,14 +31,21 @@ const handleCheckoutShopping = (e) => {
 
 
 
-  const handleIncrement = (item) => {
+const handleIncrement = (name, quantity) => {
+    dispatch(
+        updateQuantity({name,quantity}));
+
   };
 
-  const handleDecrement = (item) => {
-   
+  const handleDecrement = (name, quantity) => {
+   if(quantity>=0){
+    dispatch(
+        updateQuantity({name,quantity}));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({item}));
   };
 
   // Calculate total cost based on quantity for an item
@@ -56,9 +63,9 @@ const handleCheckoutShopping = (e) => {
               <div className="cart-item-name">{item.name}</div>
               <div className="cart-item-cost">{item.cost}</div>
               <div className="cart-item-quantity">
-                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
+                <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item.name,parseFloat(item.quantity)-1)}>-</button>
                 <span className="cart-item-quantity-value">{item.quantity}</span>
-                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item.name, parseFloat(item.quantity)+1)}>+</button>
               </div>
               <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
               <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
